eaa067c6588aac98309d53fad1a16ad0b7d4cbb2
mariarumori <rumorimaria@gmail.com>
2025-06-18 16:02:16 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,15 +4,18 @@ let handPose;
 let hands = [];
 let faceMesh;
 let faces = [];
-let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
-let sfasamento = 10;
+let sfasamento = 1;
 let scaleFactor = 1;
-let rotationAngle = 0;
-let grabActive = false;
+let grid_rows = 10;
+let grid_cols = 10;
 
 function preload() {
   handPose = ml5.handPose({ flipped: false });
-  faceMesh = ml5.faceMesh(options);
+  faceMesh = ml5.faceMesh({
+    maxFaces: 1,
+    refineLandmarks: false,
+    flipHorizontal: false,
+  });
 }
 
 function setup() {
@@ -32,48 +35,22 @@ function draw() {
   background(0);
   image(video, 0, 0, width, height);
 
-  grabActive = false;
-
-  for (let hand of hands) {
-    const kp = (name) => hand.keypoints.find((p) => p.name === name);
-
-    let indexTip = kp("index_finger_tip");
-    let indexBase = kp("index_finger_mcp");
-    let thumbTip = kp("thumb_tip");
-    let pinkyTip = kp("pinky_tip");
-
-    if (thumbTip && pinkyTip && indexTip && indexBase) {
-      let distance = dist(indexTip.x, indexTip.y, thumbTip.x, thumbTip.y);
-
-      // Mano destra (pollice a sinistra del mignolo)
-      if (thumbTip.x < pinkyTip.x) {
-        // Calcolo della rotazione
-        let dir = createVector(
-          indexTip.x - indexBase.x,
-          indexTip.y - indexBase.y
-        );
-        rotationAngle = dir.heading();
-
-        // Se distanza tra indice e pollice è piccola → mano chiusa
-        if (distance < 10) {
-          grabActive = true;
-        }
-      }
-      // Mano sinistra
-      else {
-        sfasamento = map(indexTip.x, 0, video.width, 0, 100);
-        scaleFactor = map(indexTip.y, 0, video.height, 2, 0.5);
-        scaleFactor = constrain(scaleFactor, 0.5, 2);
-      }
+  if (hands.length > 0) {
+    let hand = hands[0];
+    let indexTip = hand.keypoints.find((p) => p.name === "index_finger_tip");
+    if (indexTip) {
+      sfasamento = map(indexTip.x, 0, video.width, 0.5, 2);
+      scaleFactor = map(indexTip.y, 0, video.height, 2, 0.5);
+      scaleFactor = constrain(scaleFactor, 0.5, 2);
     }
   }
 
   for (let i = 0; i < faces.length; i++) {
-    drawUniqueVideoGridOnFace(faces[i], 5, 6);
+    drawUniqueVideoGridOnFace(faces[i], grid_cols, grid_rows);
   }
 }
 
-function drawUniqueVideoGridOnFace(face, cols = 5, rows = 6) {
+function drawUniqueVideoGridOnFace(face, cols, rows) {
   let left = face.box.xMin;
   let top = face.box.yMin;
   let boxWidth = face.box.width;
@@ -82,43 +59,35 @@ function drawUniqueVideoGridOnFace(face, cols = 5, rows = 6) {
   let baseCellW = boxWidth / cols;
   let baseCellH = boxHeight / rows;
 
-  let cellW = baseCellW * scaleFactor;
-  let cellH = baseCellH * scaleFactor;
-
-  let centerX = left + boxWidth / 2;
-  let centerY = top + boxHeight / 2;
+  let grid_center = createVector(left + boxWidth / 2, top + boxHeight / 2);
 
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      let cx = left + baseCellW * x;
-      let cy = top + baseCellH * y;
+      let cellX = left + baseCellW * x;
+      let cellY = top + baseCellH * y;
 
-      let targetX = cx;
-      let targetY = cy;
+      let cell_center = createVector(
+        cellX + baseCellW / 2,
+        cellY + baseCellH / 2
+      );
+      let diff = p5.Vector.sub(cell_center, grid_center);
+      let m = map(faces, 0, width, 0.5, 1.5);
+      let source_center = p5.Vector.add(grid_center, diff.mult(sfasamento));
 
-      // Se la mano è chiusa, porta i tasselli verso il centro
-      if (grabActive) {
-        let lerpAmt = 0.85;
-        targetX = lerp(cx, centerX, lerpAmt);
-        targetY = lerp(cy, centerY, lerpAmt);
-      }
-
-      push();
-      translate(targetX + baseCellW / 2, targetY + baseCellH / 2);
-      rotate(rotationAngle);
+      let source_x = source_center.x - baseCellW / 2;
+      let source_y = source_center.y - baseCellH / 2;
 
       copy(
         video,
-        cx - x * sfasamento,
-        cy - y * sfasamento,
-        cellW,
-        cellH,
-        -cellW / 2,
-        -cellH / 2,
-        cellW,
-        cellH
+        source_x,
+        source_y,
+        baseCellW,
+        baseCellH,
+        cellX,
+        cellY,
+        baseCellW,
+        baseCellH
       );
-      pop();
     }
   }
 }
